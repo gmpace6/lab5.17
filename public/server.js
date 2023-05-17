035b2fd0e7eb71c84725ca4ecdd667e034a1550f
Garrett Pace <elderpace6@gmail.com>
2023-05-17 15:45:58 -0600

    liveserver working

--- public/server.js
+++ public/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(4001, () => {
-    console.log('up on 4001')
+app.listen(4000, () => {
+    console.log('up on 4000')
 })
+
+
