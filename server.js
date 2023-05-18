3f7ce8f90651c76bb39aa2b6f110410f7c489041
Garrett Pace <elderpace6@gmail.com>
2023-05-18 14:14:36 -0600

    thursday lab added rollbar

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express()
 
 // app.use(express.json())
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '2ece5a1c5eb94c68aff4dd9940c7fc3d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static(__dirname + '/public'))
 
 app.listen(4000, () => {
